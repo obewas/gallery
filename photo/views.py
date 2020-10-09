896130459aeb089f2f557440cf18b6a5a125ca19
obewas <obewas1202@gmail.com>
2020-10-09 09:09:37 +0300

    added views

--- photo/views.py
+++ photo/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from .models import Image
 
 # Create your views here.
+#function that serves the welcome page
+def index(request):
+
+    # get all current photos by the latest
+    all_photos = Image.objects.all().order_by('-id')
+    return render(request, 'photo/index.html', {'all_photos':all_photos})
 def upload_photo(request):
     image = Image
 
